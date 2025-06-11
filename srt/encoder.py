7b36a10a2c8f174afcf5119e6ac9104e673ddc79
Adastra124 <chuqiuer2000@163.com>
2025-06-11 17:37:30 +0800

    Add files via upload

---
+++ srt/encoder.py
@@ -0,0 +1,427 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import math
+from typing import Type, Callable, Tuple
+from timm.models.layers import trunc_normal_, Mlp, DropPath
+
+# 网格和窗口的划分和还原
+def window_partition(
+        input: torch.Tensor,
+        window_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Window partition function.
+
+    Args:
+        input (torch.Tensor): Input tensor of the shape [B, C, H, W].
+        window_size (Tuple[int, int], optional): Window size to be applied. Default (7, 7)
+
+    Returns:
+        windows (torch.Tensor): Unfolded input tensor of the shape [B * windows, window_size[0], window_size[1], C].
+    """
+    # Get size of input
+    B, C, H, W = input.shape
+    # print("input:",input.shape)
+    # Unfold input
+    windows = input.view(B, C, H // window_size[0], window_size[0], W // window_size[1], window_size[1])
+    # Permute and reshape to [B * windows, window_size[0], window_size[1], channels]
+    windows = windows.permute(0, 2, 4, 3, 5, 1).contiguous().view(-1, window_size[0], window_size[1], C)
+    return windows
+
+
+def window_reverse(
+        windows: torch.Tensor,
+        original_size: Tuple[int, int],
+        window_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Reverses the window partition.
+
+    Args:
+        windows (torch.Tensor): Window tensor of the shape [B * windows, window_size[0], window_size[1], C].
+        original_size (Tuple[int, int]): Original shape.
+        window_size (Tuple[int, int], optional): Window size which have been applied. Default (7, 7)
+
+    Returns:
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
+    """
+    # Get height and width
+    H, W = original_size
+    # Compute original batch size
+    B = int(windows.shape[0] / (H * W / window_size[0] / window_size[1]))
+    # Fold grid tensor
+    output = windows.view(B, H // window_size[0], W // window_size[1], window_size[0], window_size[1], -1)
+    output = output.permute(0, 5, 1, 3, 2, 4).contiguous().view(B, -1, H, W)
+    return output
+
+
+def grid_partition(
+        input: torch.Tensor,
+        grid_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Grid partition function.
+
+    Args:
+        input (torch.Tensor): Input tensor of the shape [B, C, H, W].
+        grid_size (Tuple[int, int], optional): Grid size to be applied. Default (7, 7)
+
+    Returns:
+        grid (torch.Tensor): Unfolded input tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
+    """
+    # Get size of input
+    B, C, H, W = input.shape
+    # Unfold input
+    grid = input.view(B, C, grid_size[0], H // grid_size[0], grid_size[1], W // grid_size[1])
+    # Permute and reshape [B * (H // grid_size[0]) * (W // grid_size[1]), grid_size[0], window_size[1], C]
+    grid = grid.permute(0, 3, 5, 2, 4, 1).contiguous().view(-1, grid_size[0], grid_size[1], C)
+    return grid
+
+
+def grid_reverse(
+        grid: torch.Tensor,
+        original_size: Tuple[int, int],
+        grid_size: Tuple[int, int] = (7, 7)
+) -> torch.Tensor:
+    """ Reverses the grid partition.
+
+    Args:
+        Grid (torch.Tensor): Grid tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
+        original_size (Tuple[int, int]): Original shape.
+        grid_size (Tuple[int, int], optional): Grid size which have been applied. Default (7, 7)
+
+    Returns:
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
+    """
+    # Get height, width, and channels
+    (H, W), C = original_size, grid.shape[-1]
+    # Compute original batch size
+    B = int(grid.shape[0] / (H * W / grid_size[0] / grid_size[1]))
+    # Fold grid tensor
+    output = grid.view(B, H // grid_size[0], W // grid_size[1], grid_size[0], grid_size[1], C)
+    output = output.permute(0, 5, 3, 1, 4, 2).contiguous().view(B, C, H, W)
+    return output
+
+# 获得相对位置编码
+def get_relative_position_index(
+        win_h: int,
+        win_w: int
+) -> torch.Tensor:
+    """ Function to generate pair-wise relative position index for each token inside the window.
+        Taken from Timms Swin V1 implementation.
+
+    Args:
+        win_h (int): Window/Grid height.
+        win_w (int): Window/Grid width.
+
+    Returns:
+        relative_coords (torch.Tensor): Pair-wise relative position indexes [height * width, height * width].
+    """
+    coords = torch.stack(torch.meshgrid([torch.arange(win_h), torch.arange(win_w)]))
+    coords_flatten = torch.flatten(coords, 1)
+    relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+    relative_coords = relative_coords.permute(1, 2, 0).contiguous()
+    relative_coords[:, :, 0] += win_h - 1
+    relative_coords[:, :, 1] += win_w - 1
+    relative_coords[:, :, 0] *= 2 * win_w - 1
+    return relative_coords.sum(-1)
+
+# self_attention 核心模块
+class RelativeSelfAttention(nn.Module):
+    """ Relative Self-Attention similar to Swin V1. Implementation inspired by Timms Swin V1 implementation.
+
+    Args:
+        in_channels (int): Number of input channels.
+        num_heads (int, optional): Number of attention heads. Default 32
+        grid_window_size (Tuple[int, int], optional): Grid/Window size to be utilized. Default (7, 7)
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        drop (float, optional): Dropout ratio of output. Default: 0.0
+    """
+
+    def __init__(
+            self,
+            in_channels: int,
+            num_heads: int = 32,
+            grid_window_size: Tuple[int, int] = (7, 7),
+            attn_drop: float = 0.,
+            drop: float = 0.
+    ) -> None:
+        """ Constructor method """
+        # Call super constructor
+        super(RelativeSelfAttention, self).__init__()
+        # Save parameters
+        self.in_channels: int = in_channels
+        self.num_heads: int = num_heads
+        self.grid_window_size: Tuple[int, int] = grid_window_size
+        self.scale: float = num_heads ** -0.5
+        self.attn_area: int = grid_window_size[0] * grid_window_size[1]
+        # Init layers
+        self.qkv_mapping = nn.Linear(in_features=in_channels, out_features=3 * in_channels, bias=True)
+        self.attn_drop = nn.Dropout(p=attn_drop)
+        self.proj = nn.Linear(in_features=in_channels, out_features=in_channels, bias=True)
+        self.proj_drop = nn.Dropout(p=drop)
+        self.softmax = nn.Softmax(dim=-1)
+        # Define a parameter table of relative position bias, shape: 2*Wh-1 * 2*Ww-1, nH
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * grid_window_size[0] - 1) * (2 * grid_window_size[1] - 1), num_heads))
+
+        # Get pair-wise relative position index for each token inside the window
+        self.register_buffer("relative_position_index", get_relative_position_index(grid_window_size[0],
+                                                                                    grid_window_size[1]))
+        # Init relative positional bias
+        trunc_normal_(self.relative_position_bias_table, std=.02)
+
+    def _get_relative_positional_bias(
+            self
+    ) -> torch.Tensor:
+        """ Returns the relative positional bias.
+
+        Returns:
+            relative_position_bias (torch.Tensor): Relative positional bias.
+        """
+        relative_position_bias = self.relative_position_bias_table[
+            self.relative_position_index.view(-1)].view(self.attn_area, self.attn_area, -1)
+        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()
+        return relative_position_bias.unsqueeze(0)
+
+    def forward(
+            self,
+            input: torch.Tensor
+    ) -> torch.Tensor:
+        """ Forward pass.
+
+        Args:
+            input (torch.Tensor): Input tensor of the shape [B_, N, C].
+
+        Returns:
+            output (torch.Tensor): Output tensor of the shape [B_, N, C].
+        """
+        # Get shape of input
+        B_, N, C = input.shape
+        # Perform query key value mapping
+        qkv = self.qkv_mapping(input).reshape(B_, N, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        # Scale query
+        q = q * self.scale
+        # Compute attention maps
+        attn = self.softmax(q @ k.transpose(-2, -1) + self._get_relative_positional_bias())
+        # Map value with attention maps
+        output = (attn @ v).transpose(1, 2).reshape(B_, N, -1)
+        # Perform final projection and dropout
+        output = self.proj(output)
+        output = self.proj_drop(output)
+        return output
+    
+# Transformer 核心模块
+class MaxViTTransformerBlock(nn.Module):
+    """ MaxViT Transformer block.
+
+        With block partition:
+        x ← x + Unblock(RelAttention(Block(LN(x))))
+        x ← x + MLP(LN(x))
+
+        With grid partition:
+        x ← x + Ungrid(RelAttention(Grid(LN(x))))
+        x ← x + MLP(LN(x))
+
+        Layer Normalization (LN) is applied after the grid/window partition to prevent multiple reshaping operations.
+        Grid/window reverse (Unblock/Ungrid) is performed on the final output for the same reason.
+
+    Args:
+        in_channels (int): Number of input channels.
+        partition_function (Callable): Partition function to be utilized (grid or window partition).
+        reverse_function (Callable): Reverse function to be utilized  (grid or window reverse).
+        num_heads (int, optional): Number of attention heads. Default 32
+        grid_window_size (Tuple[int, int], optional): Grid/Window size to be utilized. Default (7, 7)
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        drop (float, optional): Dropout ratio of output. Default: 0.0
+        drop_path (float, optional): Dropout ratio of path. Default: 0.0
+        mlp_ratio (float, optional): Ratio of mlp hidden dim to embedding dim. Default: 4.0
+        act_layer (Type[nn.Module], optional): Type of activation layer to be utilized. Default: nn.GELU
+        norm_layer (Type[nn.Module], optional): Type of normalization layer to be utilized. Default: nn.BatchNorm2d
+    """
+
+    def __init__(
+            self,
+            in_channels: int,
+            partition_function: Callable,
+            reverse_function: Callable,
+            num_heads: int = 32,
+            grid_window_size: Tuple[int, int] = (8, 9),
+            attn_drop: float = 0.,
+            drop: float = 0.,
+            drop_path: float = 0.,
+            mlp_ratio: float = 4.,
+            act_layer: Type[nn.Module] = nn.GELU,
+            norm_layer: Type[nn.Module] = nn.LayerNorm,
+    ) -> None:
+        """ Constructor method """
+        super(MaxViTTransformerBlock, self).__init__()
+        # Save parameters
+        self.partition_function: Callable = partition_function
+        self.reverse_function: Callable = reverse_function
+        self.grid_window_size: Tuple[int, int] = grid_window_size
+        # Init layers
+        self.norm_1 = norm_layer(in_channels)
+        self.attention = RelativeSelfAttention(
+            in_channels=in_channels,
+            num_heads=num_heads,
+            grid_window_size=grid_window_size,
+            attn_drop=attn_drop,
+            drop=drop
+        )
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm_2 = norm_layer(in_channels)
+        self.mlp = Mlp(
+            in_features=in_channels,
+            hidden_features=int(mlp_ratio * in_channels),
+            act_layer=act_layer,
+            drop=drop
+        )
+        self.linear = nn.Linear(in_features=in_channels, out_features=in_channels)
+            
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """ Forward pass.
+
+        Args:
+            input (torch.Tensor): Input tensor of the shape [B, C_in, H, W].
+
+        Returns:
+            output (torch.Tensor): Output tensor of the shape [B, C_out, H (// 2), W (// 2)].
+        """
+        # Save original shape
+        B, C, H, W = input.shape
+        # 自适应窗口大小
+
+        # Perform partition
+        input_partitioned = self.partition_function(input, self.grid_window_size) #(n_view*n_patch,win_size1,winsize2,c)
+        input_partitioned = input_partitioned.view(-1, self.grid_window_size[0] * self.grid_window_size[1], C) #(n_view*n_patch,win_size1*winsize2,c)
+        # Perform normalization, attention, and dropout
+        output = input_partitioned + self.drop_path(self.attention(self.norm_1(input_partitioned)))
+        # Perform normalization, MLP, and dropout
+        output = output + self.drop_path(self.mlp(self.norm_2(output)))
+        # Reverse partition
+        output = self.reverse_function(output, (H, W), self.grid_window_size) #(10, 64, 16, 16)
+        output = self.linear(output.permute(0,2,3,1)).permute(0,3,1,2)
+        return output
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hid_dim, dp_rate):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(dim, hid_dim)
+        self.fc2 = nn.Linear(hid_dim, dim)
+        self.dp = nn.Dropout(dp_rate)
+        self.activ = nn.ReLU()
+
+    def forward(self, x):
+        x = self.dp(self.activ(self.fc1(x)))
+        x = self.dp(self.fc2(x))
+        return x
+    
+class Attention(nn.Module):
+    def __init__(self, dim, n_heads, dp_rate, attn_mode="qk", pos_dim=None):
+        super(Attention, self).__init__()
+
+        self.q_fc = nn.Linear(dim, dim, bias=False)
+        self.k_fc = nn.Linear(dim, dim, bias=False)
+        self.v_fc = nn.Linear(dim, dim, bias=False)
+        self.out_fc = nn.Linear(dim, dim)
+        self.dp = nn.Dropout(dp_rate)
+        self.n_heads = n_heads
+        self.attn_mode = attn_mode
+
+    def forward(self, x, pos=None, ret_attn=False):
+        q = self.q_fc(x) #(n_rays,n_samples+1,C)
+        q = q.view(x.shape[0], x.shape[1], self.n_heads, -1).permute(0, 2, 1, 3)#(n_rays,n_heads,n_samples+1,C/n_heads)
+        k = self.k_fc(x)
+        k = k.view(x.shape[0], x.shape[1], self.n_heads, -1).permute(0, 2, 1, 3)
+        v = self.v_fc(x)
+        v = v.view(x.shape[0], x.shape[1], self.n_heads, -1).permute(0, 2, 1, 3)
+        attn = torch.matmul(q, k.transpose(-2, -1)) / np.sqrt(q.shape[-1])#(n_rays,n_heads,n_samples+1,n_samples+1)
+        attn = torch.softmax(attn, dim=-1)
+        attn = self.dp(attn)
+
+        out = torch.matmul(attn, v).permute(0, 2, 1, 3).contiguous() #(n_rays, n_samples+1,n_heads,c)
+        out = out.view(x.shape[0], x.shape[1], -1)#(n_rays, n_samples+1,C)
+        out = self.dp(self.out_fc(out))
+        return out
+
+
+# Ray Transformer
+class View_Transformer(nn.Module):
+    def __init__(
+        self, dim, ff_hid_dim, ff_dp_rate, n_heads, attn_dp_rate, attn_mode="qk", pos_dim=None
+    ):
+        super(View_Transformer, self).__init__()
+        self.attn_norm = nn.LayerNorm(dim, eps=1e-6)
+        self.ff_norm = nn.LayerNorm(dim, eps=1e-6)
+
+        self.ff = FeedForward(dim, ff_hid_dim, ff_dp_rate)
+        self.attn = Attention(dim, n_heads, attn_dp_rate, attn_mode, pos_dim)
+
+    def forward(self, x, pos=None, ret_attn=False):
+        residue = x
+        x = self.attn_norm(x)
+        x = self.attn(x, pos, ret_attn)
+        x = x + residue
+
+        residue = x
+        x = self.ff_norm(x)
+        x = self.ff(x)
+        x = x + residue
+        return x
+
+class GLNT_E(nn.Module):
+    def __init__(self,):
+        super(GLNT_E, self).__init__()
+
+        self.view_Transformers = nn.ModuleList([])
+        self.win_transformers = nn.ModuleList([])
+        self.grid_transformers = nn.ModuleList([])
+
+        for num in range(8):
+            win_transformer = MaxViTTransformerBlock(in_channels=32, partition_function=window_partition,
+                                                reverse_function=window_reverse)
+            grid_transformer = MaxViTTransformerBlock(in_channels=32, partition_function=grid_partition,
+                                                reverse_function=grid_reverse)
+            view_Transformer = View_Transformer(dim=32, ff_hid_dim=int(32 * 4), ff_dp_rate=0.1, n_heads=4, attn_dp_rate=0.1)
+            self.win_transformers.append(win_transformer)
+            self.grid_transformers.append(grid_transformer)
+            self.view_Transformers.append(view_Transformer)
+
+        embedding_stdev = 1.
+        self.camera_embedding = nn.Parameter(torch.randn(1, 1, 32) * embedding_stdev)
+        # self.non_canonical_camera_embedding = nn.Parameter(torch.randn(1, 1, 768) * embedding_stdev)
+
+    def forward(self, img_feat):
+        """
+        Args:
+            images: [N_view, 32, height, width].
+                Assume the first image is canonical - shuffling happens in the data loader.
+        Returns:
+            scene representation: [batch_size, num_patches, channels_per_patch]
+        """
+
+        N,C,H,W = img_feat.shape
+        img_feat = img_feat.flatten(2,3).permute(0,2,1) #(n,h*w,c)
+        canonical_idxs = torch.zeros(N)
+        canonical_idxs = canonical_idxs.unsqueeze(-1).unsqueeze(-1).to(img_feat.device)
+        camera_id_embedding = canonical_idxs * self.camera_embedding #(n,1,c)
+        img_feat += camera_id_embedding
+        img_feat = img_feat.permute(0,2,1).reshape(N,C,H,W)
+        
+        for num in range(8):
+            output = self.win_transformers[num](img_feat) #(n,c,h,w)
+            output = self.grid_transformers[num](output)
+
+            output = output.flatten(2,3).permute(2,0,1) #(h*w,n,c)
+            output = self.view_Transformers[num](output)
+
+            img_feat = output.permute(1,2,0).reshape(N,C,H,W)
+
+        return output
+
+
+
+        
+
+
+
